1ef2d1b9f06da8e0bd34a1e4d20aa40e4e9eca31
Vaibhav Kapoor <vaibhavkapoor.480@gmail.com>
2024-03-14 18:44:22 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chattify-6u3n.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
